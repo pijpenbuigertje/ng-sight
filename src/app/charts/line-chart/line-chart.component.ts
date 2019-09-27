6dda1a25ff865133d058aaa9f0712856dfd8ce83
pijpenbuigertje <ikbenlaurens2@gmail.com>
2019-09-27 16:15:29 +0200

    Grafieken erbij

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors'
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 42, 58, 69, 3, 30], label: 'Bier' },
+  { data: [42, 65, 32, 65, 46, 21], label: 'Patat' },
+  { data: [65, 23, 87, 43, 45, 2], label: 'CHICKENNNNUGGETSSS' }
+];
+
+const LINE_CHART_LABELS: string[] = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -9,7 +18,18 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
+
   ngOnInit() {
   }
-
+  
 }
