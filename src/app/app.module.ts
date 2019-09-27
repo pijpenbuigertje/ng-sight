2b5ea94fd218a9db10c528d9f9cb6e18e10e71a8
pijpenbuigertje <ikbenlaurens2@gmail.com>
2019-09-27 10:35:11 +0200

    Navbar en logo toegevoegd +merr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import bootstrap from "bootstrap";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
